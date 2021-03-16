fd461436f22aa45d8706ad70d8f7821a41eb33e8
joelcoronah <joelcoronah@gmail.com>
2021-03-16 01:09:58 -0400

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 100 Days of code
+
+Started 100 Days of code on 16/03/2021.
+
+### The challenge instructions are commented out within the first few lines of the file and right below it, is the source code.<br>
+
+## Day 1 OF 100
+
+Create a madlib in charge of concatenating strings and guess a number, guess a random number.
---
+++ code/Month 1/01_day/mablib/madlibs.py
@@ -0,0 +1,21 @@
+# # string concatenation 
+
+# youtuber = "Joel Code" # some string variable
+
+# # a few ways to do this
+
+# print("subscribe to {}".format(youtuber))
+# print("subscribe to "+ youtuber)
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjetive: ")
+
+verb1 = input('verb: ')
+verb2 = input('verb: ')
+
+famous_person = input('Famous person: ')
+
+madlib = f"Computer programming is so {adj}!, it makes me so excited all the time \
+    because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
